ce7e5a64416414124d700f3402a46f81fbb9feff
satbeka <satbeka@gmail.com>
2015-03-03 18:08:41 +0600

    030315

--- src/main/java/PhoneBookBean.java
+++ src/main/java/PhoneBookBean.java
@@ -1,16 +1,26 @@
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
-@ManagedBean(name="PhoneBookBean")
+@ManagedBean(name="PhoneBookBean", eager = true)
 @SessionScoped
-public class PhoneBookBean {
+public class PhoneBookBean implements Serializable{
+    private static final long serialVersionUID = 1L;
     String action;
-
+    public PhoneBookBean() {
+        System.out.println("PhoneBookBean started!");
+    }
     public String getAction() {
         return action;
     }
 
     public void setAction(String action) {
         this.action = action;
+    }
+
+    public String doAddAction(){
+        this.action="add";
+        System.out.println(this.action);
+        return action;
     }
 }
