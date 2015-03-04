6f5f07b965b93d8a0e9eaabdd519de88c10af40d
satbeka <satbeka@gmail.com>
2015-03-04 17:55:59 +0600

    040315

--- src/main/java/persistence/SqlUtil.java
+++ src/main/java/persistence/SqlUtil.java
@@ -30,5 +30,46 @@ public class SqlUtil {
         return phoneBooks;
     }
 
+    public static ArrayList<PhoneBook> getPhoneBook(String phoneSeek){
+        System.out.println("getPhoneBook()="+phoneSeek);
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        ArrayList<PhoneBook> phoneBooks = new ArrayList<PhoneBook>();
+
+        Query query = session.getNamedQuery("callPhoneBookByPhoneProcedure")
+                .setParameter("phoneSeek",phoneSeek);
+        List result = query.list();
+
+        for(int i=0; i<result.size(); i++){
+            PhoneBook phoneBook = (PhoneBook)result.get(i);
+            System.out.println(phoneBook.getFio());
+            phoneBooks.add(phoneBook);
+        }
+
+        session.close();
+        HibernateUtil.shutdown();
+        return phoneBooks;
+    }
+    public static ArrayList<PhoneBook> getPhoneBookAll(){
+        System.out.println("getPhoneBookAll()");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        ArrayList<PhoneBook> phoneBooks = new ArrayList<PhoneBook>();
+
+        Query query = session.getNamedQuery("callPhoneBookAllProcedure");
+        List result = query.list();
+
+        for(int i=0; i<result.size(); i++){
+            PhoneBook phoneBook = (PhoneBook)result.get(i);
+            System.out.println(phoneBook.getFio());
+            phoneBooks.add(phoneBook);
+        }
+
+        session.close();
+        HibernateUtil.shutdown();
+        return phoneBooks;
+    }
 
 }
